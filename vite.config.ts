dfc3f5ffe07852fb4d7da223f3233b64bc703625
Ap01lY0n <128406680+neverm8re@users.noreply.github.com>
2025-02-13 11:07:56 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,6 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base:"/valentine",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
